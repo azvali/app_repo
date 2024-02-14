d4bc3a5040e26f9317c5c2856f4f594d006bdfef
Yousef Mardrosyan <104654454+azvali@users.noreply.github.com>
2024-02-14 16:06:41 -0500

    e

--- js/index.js
+++ js/index.js
@@ -4,12 +4,16 @@ const submit_button = document.getElementById("submit_button");
 
 
 function button_click(){
+    output_content.value = "";
     const get_input = input_content.value;
 
     console.log("Input value:", get_input);
 
-    output_content.textContent = get_input;
+    output_content.value = get_input;
     input_content.value = "";
+
+    document.getElementById('display_box').textContent = 'Test Message';
+
 }
 
 submit_button.addEventListener("click",button_click);
